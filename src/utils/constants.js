d1c4f0acc07d1fa6c6656cc9e3113fd928587f05
pushpiinder <pushpinder1997.ps@gmail.com>
2024-02-27 22:17:59 +0530

    The end game!
    all changes of Part 3 and Part 4

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,7 @@ export const YOUTUBE_VIDEOS_API =
 
 export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const LIVE_CHAT_COUNT = 25;
+
+// Live Chat >>>> Infinite Scroll >>>>>> Pagination
